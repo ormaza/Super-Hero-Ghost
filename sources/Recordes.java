228a426b15bfe0e793bb6bc5932a63aab8dd3f1e
Ormazabal Nascimento <ormazaball@yahoo.com.br>
2018-07-12 00:22:10 -0300

    Update Recordes.java

--- sources/Recordes.java
+++ sources/Recordes.java
@@ -21,25 +21,25 @@ public class Recordes extends JFrame{
     private JTable jtable;
     private DefaultTableModel table;
     private String[] nomes = new String[50];
-	private int[] pontos = new int[50];
+    private int[] pontos = new int[50];
     private int numCadastrados=1;
     private String[] cols = new String[2];
     
     public void initTable() {
     	table =  new DefaultTableModel(cols,0);
-		jtable =  new JTable(table);
+	jtable =  new JTable(table);
                 
         setSize(200, 500);
-		setLocation(50,80);
+	setLocation(50,80);
         setResizable(false);
                 
         JScrollPane scroll = new JScrollPane(jtable, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-		JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		getContentPane().add(scroll, BorderLayout.CENTER);
+	JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+	getContentPane().add(scroll, BorderLayout.CENTER);
                 
         painel = new JPanel();
         painel.add(jtable);
-		add(painel);
+	add(painel);
 
         jtable.setSelectionBackground(Color.ORANGE);
     }
@@ -54,8 +54,8 @@ public class Recordes extends JFrame{
           key = pontos[j];
           nome = nomes[j];
           for (i = j - 1; (i >= 0) && (pontos[i] > key); i--) {
-        	  pontos[i + 1] = pontos[i];
-        	  nomes[i + 1] = nomes[i];
+		  pontos[i + 1] = pontos[i];
+		  nomes[i + 1] = nomes[i];
           }
           pontos[i + 1] = key;
           nomes [i + 1] = nome;
@@ -65,15 +65,15 @@ public class Recordes extends JFrame{
     public void listPlayers() {
     	String nome, ponto;
     	int numRow=0;
-		String[] row = {cols[0],cols[1]};
-		table.insertRow(numRow, row);
-		insertionSort();
-		for(numRow=1;numRow<=numCadastrados;numRow++) {
-			nome = nomes[numCadastrados-numRow];
-			ponto = ""+pontos[numCadastrados-numRow];
-			String[] row2 = {nome,ponto};
-            table.insertRow(numRow, row2);
-		}
+	String[] row = {cols[0],cols[1]};
+	table.insertRow(numRow, row);
+	insertionSort();
+	for(numRow=1;numRow<=numCadastrados;numRow++) {
+		nome = nomes[numCadastrados-numRow];
+		ponto = ""+pontos[numCadastrados-numRow];
+		String[] row2 = {nome,ponto};
+    		table.insertRow(numRow, row2);
+	}
     }
     
     public void addRecord(String nome, int ponto) {
@@ -85,10 +85,10 @@ public class Recordes extends JFrame{
     public Recordes() {
         super("RECORDES");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-	    cols[0] = "Nome";
-	    cols[1] = "Pontos";
-		initTable();
-		listPlayers();
+	cols[0] = "Nome";
+	cols[1] = "Pontos";
+	initTable();
+	listPlayers();
     }
      
 }
